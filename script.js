073b6fe1f2abe7fab296af04514bb776df2e44b8
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-01-07 10:00:17 +0530

    F2 1st contest

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee.name);
+    }
+  });
+
+
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee.name);
+    }
+  });
 }
 
 function addData() {
   //Write your code here
+  arr.push({id: 4, name: "susan", age: "20", profession: "intern"});
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  arr = arr.filter(employee => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+    { id: 4, name: "susan", age: "20", profession: "intern" },
+    { id: 5, name: "mike", age: "22", profession: "developer" },
+    { id: 6, name: "lisa", age: "21", profession: "manager" }
+  ];
+  let combinedArray = arr.concat(arr2);
+  console.log(combinedArray);
 }
